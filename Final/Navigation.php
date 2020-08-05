44650e5aa0e62e6e3a8e0c58075b5ca339f9f8c1
bhahn12 <68400995+bhahn12@users.noreply.github.com>
2020-08-05 16:38:29 -0400

    Update Navigation.php

--- Final/Navigation.php
+++ Final/Navigation.php
@@ -6,7 +6,7 @@
         <title>Navigation</title>
 <div class = "bar">
 <ul>
-<li><a href="lawnindex.php">Home</a> </li>
+<li><a href="index.php">Home</a> </li>
 <li><a href="about.php">About Us</a> </li>
 <li><a href="pricing.php">Pricing Chart</a> </li>
 <li><a href="services.php">Other Services</a> </li>
