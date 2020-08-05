a8125e0070dde9cf86c47dfbe0823596bdbae64e
bhahn12 <68400995+bhahn12@users.noreply.github.com>
2020-08-05 16:51:50 -0400

    Update services.php

--- Final/services.php
+++ Final/services.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="lawnstylesheet.css">
-        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>SERVICES</title>
     </head>
     <body>
@@ -18,7 +18,7 @@
         can give your lawn.
     </p>
 
-    <h2>Weed Contorl</h2>
+    <h2>Weed Control</h2>
     <p class = "example center_text">We offer coming out and spraying weeds in the cracks of your sidewalk,
         driveway, and any other area they grow.
     </p>
@@ -26,7 +26,7 @@
     <p class = "example center_text">We offer our services in the fall to come and clean up all debris that has fallen
         from trees. 
     </p>
-    <p class = "example center_text">*Please use the contact form for pricing of our additional services. 
+    <p class = "center_text">*Please use the contact form for pricing of our additional services. 
     </p>
     <footer>
     <p>Company &copy; Bob's Lawn Care. All rights reserved.</p>
